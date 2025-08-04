d3b1d41a398e285eb68c4661b54d500fb3c3461e
RashidTortuga <seniorsamuel321@gmail.com>
2025-08-04 23:28:32 +0300

    add postlist component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import Counter from './components/Counter'
 import PostItem from './components/PostItem'
+import PostList from './components/PostList'
 import './styles/App.css'
 function App() {
   const [value, setValue] = useState('Текст')
-
+  const posts = [
+    { id: 1, title: 'JS', body: 'Description' },
+    { id: 2, title: 'JS2', body: 'Description' },
+    { id: 3, title: 'JS3', body: 'Description' },
+    { id: 4, title: 'JS4', body: 'Description' },
+  ]
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: 'JS', body: 'Description' }} />
-      <PostItem post={{ id: 2, title: 'TS', body: 'Description' }} />
-      <PostItem post={{ id: 3, title: 'React', body: 'Description' }} />
     </div>
   )
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PostItem = (props) => {
   console.log(props)
   return (
-    <div className="post">
+    <div style={{ display: 'flex' }} className="post">
       <div className="post__content">
         <strong>
-          {props.post.id}.
-          {props.post.title}
+          {props.post.id}.{props.post.title}
         </strong>
         <div>{props.post.body}</div>
       </div>
       <div className="post__btns">
-        <button onClick={PostItem={display=none}}>Delete</button>
+        <button >Delete</button>
       </div>
     </div>
   )
