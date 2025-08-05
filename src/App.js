0f0dbb26a3563b4dea45d546ca18ad9534e4feb5
RashidTortuga <seniorsamuel321@gmail.com>
2025-08-06 00:19:30 +0300

    add input and button components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import './styles/App.css'
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import Counter from './components/Counter'
 import PostItem from './components/PostItem'
 import PostList from './components/PostList'
-import './styles/App.css'
+import MyButton from './components/UI/button/MyButton'
+import MyInput from './components/UI/input/MyInput'
 function App() {
   const [value, setValue] = useState('Текст')
   const posts = [
@@ -12,16 +14,18 @@ function App() {
     { id: 3, title: 'JS3', body: 'Description' },
     { id: 4, title: 'JS4', body: 'Description' },
   ]
-  const postes = [
-    { id: 1, title: 'ляшко', body: 'Description' },
-    { id: 2, title: 'тимошенко', body: 'Description' },
-    { id: 3, title: 'яникевич', body: 'Description' },
-    { id: 4, title: 'якубович', body: 'Description' },
-  ]
-  return (
+  const [title,setTitle] = useState ('')
+ const addNewPost =()=>{
+
+ }
+   return (
     <div className="App">
-<PostList posts={posts} title={'Погромисты'}/>
-<PostList posts={postes} title={'Партия рыгона'}/>
+      <form>
+        <MyInput value={title} onChange={e =>setTitle(e.target.value)} type="text" placeholder="Название поста" />
+        <MyInput value={title} onChange={e =>setTitle(e.target.value)}  onChange={e =>setTitle(e.target.value)} type="text" placeholder="Описание поста" />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Погромисты" />
     </div>
   )
 }
